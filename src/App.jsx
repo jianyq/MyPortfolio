b837bd46ebd2892785f0f7f27f52ce8982a5ab59
jianyq <jianyq2003@gmail.com>
2023-07-06 14:35:24 -0500

    set

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { About, Contact, Education, Experience, Feedbacks, Hero, Navbar, Tech, W
 const App = () => {
   return (
     <BrowserRouter>
+    
       <div className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
